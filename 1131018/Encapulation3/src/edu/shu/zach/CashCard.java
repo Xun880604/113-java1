73d935c796c6527c9cdc6641f170cbeb11501ee7
Xun880604 <a112221013@mail.shu.edu.tw>
2024-10-18 12:01:10 +0800

    Add files via upload

---
+++ 1131018/Encapulation3/src/edu/shu/zach/CashCard.java
@@ -0,0 +1,64 @@
+package   edu.shu.zach;
+
+public class CashCard {
+    private String number;
+    private int balance;
+    private int bonus;
+
+    public CashCard(String number, int balance, int bonus) {
+        this.number = number;
+        this.balance = balance;
+        this.bonus = bonus;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public int getBonus() {
+        return this.bonus;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+
+    public void store(int money) {
+        if (money > 0) {
+            balance += money;
+            if (money >= 1000) {
+                bonus += money/1000;
+            }
+        }
+        else {
+            System.out.println("儲值金額為負，來亂的!");
+
+        }
+    }
+    public void charge(int money) {
+        if (money > 0) {
+            if (money <= balance) {
+                balance -= money;
+            } else {
+                System.out.println("餘額不足，可憐啊!");
+            }
+        }
+        else {
+                System.out.println("扣除金額為負，來亂的!");
+        }
+    }
+
+    public int exchange(int bonus) {
+        if (bonus > 0) {
+            bonus -= bonus;
+        }
+        return this.bonus;
+
+    }
+}
